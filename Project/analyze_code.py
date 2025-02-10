5c3be4d27e40c2986dd277e6b9bdaa851725e1df
Randall lujan <meta2graei@gmail.com>
2025-02-09 16:52:59 -0800

    Update analyze_code.py

--- Project/analyze_code.py
+++ Project/analyze_code.py
@@ -1,1 +1,10 @@
+# analyze_code.py
+import os
 
+def analyze_code(project_path):
+    summary = "Project contains the following files:\n"
+    for root, dirs, files in os.walk(project_path):
+        for file in files:
+            if file.endswith('.py'):
+                summary += f"- {file}\n"
+    return summary
