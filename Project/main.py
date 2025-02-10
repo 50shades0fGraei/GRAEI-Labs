077e95f5d47036b1cda1f2b103b7927e0ca2f752
Randall lujan <meta2graei@gmail.com>
2025-02-09 16:52:11 -0800

    Update main.py

--- Project/main.py
+++ Project/main.py
@@ -1,1 +1,12 @@
+# main.py
+from analyze_code import analyze_code
+from generate_text import generate_text
 
+def main():
+    project_path = input("Enter the path to your project: ")
+    code_summary = analyze_code(project_path)
+    explanation_text = generate_text(code_summary)
+    print(explanation_text)
+
+if __name__ == '__main__':
+    main()
