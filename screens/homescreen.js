1afc812d0a6984ab9c7b5c94adc516a36d1fea08
Randall lujan <meta2graei@gmail.com>
2025-02-09 21:56:13 -0800

    Update homescreen.js

--- screens/homescreen.js
+++ screens/homescreen.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Button, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import SplitScreen from '../components/SplitScreen';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <SplitScreen />
+      <TouchableOpacity onPress={() => navigation.navigate('Settings')} style={styles.iconButton}>
+        <Image
+          source={require('../assets/graeilabs_icon.jpg')}
+          style={styles.icon}
+        />
+      </TouchableOpacity>
+      <Button title="Go to Program" onPress={() => navigation.navigate('Program')} />
     </View>
   );
 };
@@ -14,6 +21,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  iconButton: {
+    position: 'absolute',
+    bottom: 50,
+    right: 20,
+  },
+  icon: {
+    width: 50,
+    height: 50,
+  },
 });
+
 
 export default HomeScreen;
