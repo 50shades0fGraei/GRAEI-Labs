0f104426ff9b85aed4ad51f77bebc5fed187f366
Randall lujan <meta2graei@gmail.com>
2025-02-09 21:23:59 -0800

    Update services

---
+++ services/terminalservice.js
@@ -0,0 +1,9 @@
+// services/TerminalService.js
+
+const executeCommand = (input) => {
+  // Logic to execute a terminal command
+  // This can be an API call to your backend or some local processing
+  return `Executing command: ${input}`;
+};
+
+export default executeCommand;
